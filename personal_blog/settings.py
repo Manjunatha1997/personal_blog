99e202d1414041649cf712f3a91bad36d08188de
Manjunatha1997 <59602811+Manjunatha1997@users.noreply.github.com>
2021-03-18 21:40:31 +0530

    Update settings.py

--- personal_blog/settings.py
+++ personal_blog/settings.py
@@ -137,5 +137,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com' # ask IT people
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'lovelymanju198@gmail.com' # your email address
-EMAIL_HOST_PASSWORD = '198.manju' # your password
+EMAIL_HOST_USER = '' # your email address
+EMAIL_HOST_PASSWORD = '' # your password
